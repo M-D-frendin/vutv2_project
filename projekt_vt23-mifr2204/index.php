b3606ab8c0a7197bce2864edc24fc8f2f7812c36
David Frendin <david.frendin@gmail.com>
2024-03-17 23:31:24 +0100

    updated index

--- projekt_vt23-mifr2204/index.php
+++ projekt_vt23-mifr2204/index.php
@@ -22,6 +22,7 @@ $posts = Post::allPostsWithLimit(5); //skapar en lista med Post instanser med al
     <?php
     //visa sidemenu
     include('includes/sidemenu.php');
+    /*
     ?>
     <div class="articlegrid">
     <?php
@@ -44,8 +45,34 @@ $posts = Post::allPostsWithLimit(5); //skapar en lista med Post instanser med al
         </article>
     <?php
     }
+*/
+?>
+<div class="index-article-list">
+
+<?php
+    //visa alla Posts
+    foreach($posts as $post) {
+    ?>
+        <article>
+
+        <h2><a href="posts.php?id=<?= $post->id; ?>"><?= $post->title; ?></a></h2>
+
+        <p class="crebyp">Skapad <?= $post->created; ?> </p>
+        <div class="by">
+            <p>Av:: </p>
+            <p class="username"> <?= $post->getUser()->username;?> </p>
+        </div>
+ 
+        <div class="content">
+            <?= $post->content; ?>
+        </div>
+        </article>
+    <?php
+    }
 
 ?>
+
+
     </div>
 
 <?php
